9e49a78900cff58340f68866aa992527e7f3ff67
Hüseyin Bilen <huseyinxbilen@gmail.com>
2022-05-27 00:01:00 +0300

    Update Feature Added

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -6,5 +6,7 @@ const router = express.Router();
 
 router.route('/').get(portfolioController.getPortfolio);
 router.route('/add').post(portfolioController.addNewItem);
+router.route('/items/:id').delete(portfolioController.deleteItem);
+router.route('/items/:id').put(portfolioController.updateItem);
 
 module.exports = router;
